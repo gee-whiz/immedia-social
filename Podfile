ec792dd349cdbc4cd8ee010f8f756c4fdbb4b1e2
George <george@immedia.co.za>
2017-03-23 08:27:52 +0200

    initial commit

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'immedia social' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for immedia social
+
+  target 'immedia socialTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'immedia socialUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ immedia social/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  immedia social
+//
+//  Created by George Kapoya on 2017/03/23.
+//  Copyright © 2017 immedia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
